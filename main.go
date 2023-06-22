3907b5f8ffbb0cfd266240b7f12a5b29e8dbf29a
Mohamed Belgaied Hassine <belgaied2@hotmail.com>
2023-06-22 21:40:26 +0200

    Adding some inline comments
    Signed-off-by: Mohamed Belgaied Hassine <belgaied2@hotmail.com>

--- main.go
+++ main.go
@@ -26,7 +26,9 @@ func main() {
 		panic(err)
 	}
 
-	//create a new clientFactory
+	//create a new clientFactory, this is used to create clients for different resources
+	// Since each Resource type needs its own client, the clientFactory is used to create the client for each resource.
+	// However, the clientFactory is able to return the same http.Client for each client it creates, meaning that the same http.Client is used for all clients.
 	clientFactory, err := client.NewSharedClientFactoryForConfig(clientConfig)
 	if err != nil {
 		panic(err)
@@ -38,18 +40,22 @@ func main() {
 		Kind:    "Setting",
 	}
 
+	// create a new client for the Setting resource
 	settingsClient, err := clientFactory.ForKind(settingGVK)
 	if err != nil {
 		panic(err)
 	}
 
+	// instantiate a new empty SettingList
 	settingsList := &v3.SettingList{}
 
+	// list all settings in the default namespace using the settingsClient and store the results in the settingsList
 	err = settingsClient.List(context.TODO(), "default", settingsList, v1.ListOptions{}) // this is a list of settings
 	if err != nil {
 		panic(err)
 	}
 
+	// print the name of each setting in the settingsList
 	for _, setting := range settingsList.Items {
 		println(setting.Name)
 	}
